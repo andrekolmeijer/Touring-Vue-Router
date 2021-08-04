42530ac257b0e1a26fab435e00277d7da187494e
ndrklmr <andrekolmeijer@gmail.com>
2021-08-04 14:10:33 +0200

    Added scenario for event not found

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -28,6 +28,13 @@ export default {
             })
             .catch((error) => {
                 console.log(error)
+
+                this.$router.push({
+                    name: '404Resource',
+                    params: {
+                        resource: 'event'
+                    }
+                })
             })
     }
 }
