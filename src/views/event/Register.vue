d03162bcd9b4c35e22ca5f1fde38fc14342ba033
ndrklmr <andrekolmeijer@gmail.com>
2021-08-04 13:52:17 +0200

    Prettier

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,20 +1,20 @@
 <template>
-  <p>Register for the event here</p>
-  <button @click="register">Register Me</button>
+    <p>Register for the event here</p>
+    <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event'],
-  methods: {
-    register() {
-      // Call to API
-      // If registered then redirect to event details
+    props: ['event'],
+    methods: {
+        register() {
+            // Call to API
+            // If registered then redirect to event details
 
-      this.$router.push({
-        name: 'EventDetails'
-      })
+            this.$router.push({
+                name: 'EventDetails'
+            })
+        }
     }
-  }
 }
 </script>
