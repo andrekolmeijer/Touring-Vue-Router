1675e6bfe5c9c50a28b7aa01177fe7bb400671a5
ndrklmr <andrekolmeijer@gmail.com>
2021-08-04 13:33:29 +0200

    Added button that routes back to EventDetails

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,24 @@
 <template>
     <p>Register for the event here</p>
+    <button class="button" @click="onRegister">Let's register!</button>
 </template>
 
 <script>
 export default {
-    props: ['event']
+    props: ['event'],
+    methods: {
+        onRegister() {
+            this.$router.push({
+                name: 'EventDetails'
+            })
+        }
+    }
 }
 </script>
+
+<style scoped>
+.button {
+    cursor: pointer;
+    padding: .5rem 1rem;
+}
+</style>
